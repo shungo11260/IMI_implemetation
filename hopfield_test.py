f1e84a11f3be91230470dd6a3b5fbea14bd27da5
shungo11260 <shungotaniguchi1120@gmail.com>
2017-06-27 11:52:03 +0900

    重みを作成する関数および、更新する関数の作成

--- hopfield_test.py
+++ hopfield_test.py
@@ -1,80 +1,114 @@
 
 # coding: utf-8
 
-# In[51]:
+# In[ ]:
 
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 
 
-# In[52]:
+# In[ ]:
 
 random.randrange(-1, 2, 2)
 
 
-# In[53]:
+# In[ ]:
 
-subplot(1,6,1)
+plt.subplot(1,6,1)
 train_data1 = np.ones((5,5))
 train_data1[0:5, ::4] = -1
 train_data1[1, 1::2] = -1
 train_data1[2, ::2] = -1
-imshow(train_data1, interpolation='nearest', cmap='bone')
+plt.imshow(train_data1, interpolation='nearest', cmap='bone')
 
 
-# In[54]:
+# In[ ]:
 
-subplot(1,6,2)
+plt.subplot(1,6,2)
 train_data2 = np.ones((5,5))
 train_data2[0, 2] = -1
 train_data2[1, 1::2] = -1
 train_data2[2:5, ::4] = -1
 train_data2[3] = -1
-imshow(train_data2, interpolation='nearest', cmap='bone')
+plt.imshow(train_data2, interpolation='nearest', cmap='bone')
 
 
-# In[55]:
+# In[ ]:
 
-subplot(1,6,3)
+plt.subplot(1,6,3)
 train_data3 = np.ones((5,5))
 train_data3[0] = -1
 train_data3[1:5, 2] = -1
-imshow(train_data3, interpolation='nearest', cmap='bone')
+plt.imshow(train_data3, interpolation='nearest', cmap='bone')
 
 
-# In[56]:
+# In[ ]:
 
-subplot(1,6,4)
+plt.subplot(1,6,4)
 train_data4 = np.ones((5,5))
 train_data4[0:5, ::3] = -1
 train_data4[2, :4] = -1
-imshow(train_data4, interpolation='nearest', cmap='bone')
+plt.imshow(train_data4, interpolation='nearest', cmap='bone')
 
 
-# In[57]:
+# In[ ]:
 
-subplot(1,6,5)
+plt.subplot(1,6,5)
 train_data5 = np.ones((5,5))
 train_data5[::2] = -1
 train_data5[0, 4] = 1
 train_data5[1, 0] = -1
 train_data5[3, 4] = -1
-imshow(train_data5, interpolation='nearest', cmap='bone')
+plt.imshow(train_data5, interpolation='nearest', cmap='bone')
 
 
-# In[58]:
+# In[ ]:
 
-subplot(1,6,6)
+plt.subplot(1,6,6)
 train_data6 = np.ones((5,5))
 train_data6[0:3, 1] = -1
 train_data6[4, 1] = -1
-imshow(train_data6, interpolation='nearest', cmap='bone')
+plt.imshow(train_data6, interpolation='nearest', cmap='bone')
 
 
-# In[59]:
+# In[ ]:
 
-show()
+train_data = []
+train_data.append(train_data1)
+train_data.append(train_data2)
+train_data.append(train_data3)
+train_data.append(train_data4)
+train_data.append(train_data5)
+train_data.append(train_data6)
+
+
+# In[ ]:
+
+plt.show()
+
+
+# In[ ]:
+
+def set_weight(data):
+    vector_size = data.size
+    vector = data.reshape((vector_size, 1))
+    weight = np.dot(vector, vector.T) - np.eye(25)
+    return weight
+
+
+# In[ ]:
+
+def update_weight(weight, data, n):
+    weight = (n*weight + set_weight(data)) / (n+1)
+    return weight
+
+
+# In[ ]:
+
+td_num = 1
+for i in range(td_num):
+    W = update_weight(W, train_data[i], i)
 
 
 # In[ ]:
